36d9752ba3be94db1c25c44c8e2fee2c91a92d18
Sherloque <sherloque7@gmail.com>
2021-11-10 23:48:53 +0200

    DEV-2021 Added build package

---
+++ back/app/build/precache-manifest.e5e7bc92438d3ad89846ccce72bab321.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ef1759c0f2a5837b00bfab19fa5905",
+    "url": "/index.html"
+  },
+  {
+    "revision": "831a31eef0bbebbbd521",
+    "url": "/static/css/main.deba0ef7.chunk.css"
+  },
+  {
+    "revision": "c485426672e1d76af4ac",
+    "url": "/static/js/2.134502a9.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.134502a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "831a31eef0bbebbbd521",
+    "url": "/static/js/main.12de0ff6.chunk.js"
+  },
+  {
+    "revision": "cdf041b3892fd22a989b",
+    "url": "/static/js/runtime-main.6c427cc5.js"
+  },
+  {
+    "revision": "9586293f842face120b145fd0dc66d0a",
+    "url": "/static/media/1_5bc883c28a5ea.9586293f.jpg"
+  },
+  {
+    "revision": "4788048f319dc48101678d9e69f5077e",
+    "url": "/static/media/Portrait_Placeholder.4788048f.png"
+  },
+  {
+    "revision": "2e94b7a2a95d13dfe5d30c6b844aeecd",
+    "url": "/static/media/csd_emblem updated.2e94b7a2.jpg"
+  }
+]);
